c4c71e9418b08c1408049090f64c1065cf75a481
Eloy Gómez García <eloygomezgarcia@MacBook-Air-de-Eloy.local>
2023-04-19 20:46:08 +0200

    hello world

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,3 @@
-import { NativeModulesProxy, EventEmitter, Subscription } from 'expo-modules-core';
-
-// Import the native module. On web, it will be resolved to ExpoAlerts.web.ts
-// and on native platforms to ExpoAlerts.ts
-import ExpoAlertsModule from './ExpoAlertsModule';
-import ExpoAlertsView from './ExpoAlertsView';
-import { ChangeEventPayload, ExpoAlertsViewProps } from './ExpoAlerts.types';
-
-// Get the native constant value.
-export const PI = ExpoAlertsModule.PI;
-
-export function hello(): string {
-  return ExpoAlertsModule.hello();
+export function hello(text = "hola") {
+  return text;
 }
-
-export async function setValueAsync(value: string) {
-  return await ExpoAlertsModule.setValueAsync(value);
-}
-
-const emitter = new EventEmitter(ExpoAlertsModule ?? NativeModulesProxy.ExpoAlerts);
-
-export function addChangeListener(listener: (event: ChangeEventPayload) => void): Subscription {
-  return emitter.addListener<ChangeEventPayload>('onChange', listener);
-}
-
-export { ExpoAlertsView, ExpoAlertsViewProps, ChangeEventPayload };
